6024e9e2137f8d640792c3840fe96b7c77b4fb58
MarsSkyrunner <marsskyrunner@gmail.com>
2019-01-30 09:45:06 -0800

    subject group avatar updated

--- app/src/main/java/com/mars_skyrunner/lalalog/Constants.java
+++ app/src/main/java/com/mars_skyrunner/lalalog/Constants.java
@@ -16,13 +16,21 @@ public class Constants {
     public static final String REVIEW_SUBJECT = packageName + ".REVIEW_SUBJECT";
     public static final String SUBJECT_DETAIL_MODE = packageName + ".SUBJECT_DETAIL_MODE";
 
+    public static final String NEW_RECORD = packageName + ".NEW_RECORD";
+    public static final String EDIT_RECORD_MODE = packageName + ".EDIT_RECORD_MODE";
+    public static final String REVIEW_RECORD = packageName + ".REVIEW_RECORD";
+    public static final String RECORD_DETAIL_MODE = packageName + ".RECORD_DETAIL_MODE";
+
+
+
     public static final String DELETE_SUBJECT_SERVICE_RESULT = packageName + ".DELETE_SUBJECT_SERVICE_RESULT" ;
     public static final String SAVE_RECORD = "com.mars_skyrunner.lalalog.SAVE_RECORD";
+
+    public static final String EDIT_RECORD = packageName + ".EDIT_RECORD";
     public static final int RECORD_LOADER = 100;
     public static final int SUBJECT_LOADER = 200;
     public static final int GET_RECORD_REF_LOADER = 300;
     public static final String RECORD_BUNDLE = "com.mars_skyrunner.lalalog.RECORD_BUNDLE";
-    public static final String EDIT_RECORD = "com.mars_skyrunner.lalalog.EDIT_RECORD";
     public static final String SAVE_SUBJECT = "com.mars_skyrunner.lalalog.SAVE_SUBJECT";
     public static final String SUBJECT_URI_STRING = "com.mars_skyrunner.lalalog.SUBJECT_URI_STRING";
     public static final String SUBJECT_BUNDLE =  "com.mars_skyrunner.lalalog.SUBJECT_BUNDLE";
